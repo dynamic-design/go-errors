e53b97ce62720b8fc89b452290229cd9da4fcfed
Joakim Gustin <joakim@dynamiccommunication.se>
2014-01-22 16:47:00 +0100

    Rename

--- client.go
+++ client.go
@@ -1,9 +1,15 @@
-package clientError
+package errors
 
 type ClientError struct {
 	Message string            `json:"message"`
-	Errors  []ValidationError `json:"errors"`
+	Errors  []ValidationError `json:"errors,omitempty"`
 }
+
+const (
+	Invalid       string = "invalid"
+	MissingField  string = "missing_field"
+	AlreadyExists string = "already_exists"
+)
 
 type ValidationError struct {
 	Resource string `json:"resource"`
@@ -13,4 +19,8 @@ type ValidationError struct {
 
 func (ce ClientError) HasError() bool {
 	return len(ce.Message) > 0 || len(ce.Errors) > 0
+}
+
+func (ce *ClientError) Error() string {
+	return ce.Message
 }
